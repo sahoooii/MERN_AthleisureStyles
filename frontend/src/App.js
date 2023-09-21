141d21a768107d1491a4ee7159fde3c0ed7b8503
sahoooii <saho.chu@gmail.com>
2023-09-21 12:08:15 +0900

    set the theme color and fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-		<div className='App'>
+		<div className='app'>
 			<h1>Athleisure Styles</h1>
 		</div>
 	);
---
+++ frontend/src/theme.js
@@ -0,0 +1,102 @@
+import { createTheme } from '@mui/material/styles';
+
+export const shades = {
+	primary: {
+		100: '#cccccc',
+		200: '#999999',
+		300: '#666666',
+		400: '#333333',
+		500: '#000000',
+		600: '#000000',
+		700: '#000000',
+		800: '#000000',
+		900: '#000000',
+	},
+	blue: {
+		100: '#d5e5f0',
+		200: '#accce1',
+		300: '#82b2d1',
+		400: '#5999c2',
+		500: '#2f7fb3',
+		600: '#26668f',
+		700: '#1c4c6b',
+		800: '#133348',
+		900: '#091924',
+	},
+	babyBlue: {
+		100: '#e7f5fc',
+		200: '#d0ecf9',
+		300: '#b8e2f6',
+		400: '#a1d9f3',
+		500: '#89cff0',
+		600: '#6ea6c0',
+		700: '#527c90',
+		800: '#375360',
+		900: '#1b2930',
+	},
+	babyPink: {
+		100: '#fdf3f3',
+		200: '#fbe7e7',
+		300: '#f8dada',
+		400: '#f6cece',
+		500: '#f4c2c2',
+		600: '#c39b9b',
+		700: '#927474',
+		800: '#624e4e',
+		900: '#312727',
+	},
+	neutral: {
+		100: '#f5f5f5',
+		200: '#ecebeb',
+		300: '#e2e1e1',
+		400: '#d9d7d7',
+		500: '#cfcdcd',
+		600: '#a6a4a4',
+		700: '#7c7b7b',
+		800: '#535252',
+		900: '#292929',
+	},
+};
+
+export const theme = createTheme({
+	palette: {
+		primary: {
+			main: shades.primary[500],
+		},
+		blue: {
+			dark: shades.blue[500],
+			main: shades.blue[400],
+		},
+		babyBlue: {
+			main: shades.babyBlue[500],
+		},
+		babyPink: {
+			main: shades.babyPink[500],
+		},
+		neutral: {
+			dark: shades.neutral[700],
+			main: shades.neutral[500],
+			light: shades.neutral[100],
+		},
+	},
+	typography: {
+		fontFamily: ['Ubuntu', 'sans-serif'].join(','),
+		fontSize: 11,
+		h1: {
+			fontFamily: ['Cinzel', 'sans-serif'].join(','),
+			fontSize: 48,
+		},
+		h2: {
+			fontFamily: ['Cinzel', 'sans-serif'].join(','),
+			fontSize: 36,
+		},
+		h3: {
+			fontFamily: ['Cinzel', 'sans-serif'].join(','),
+			fontSize: 20,
+		},
+		h4: {
+			fontFamily: ['Cinzel', 'sans-serif'].join(','),
+			fontSize: 14,
+		},
+	},
+});
