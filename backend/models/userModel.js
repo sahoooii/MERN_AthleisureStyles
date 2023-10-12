fef5fe1b1732b6f811074ddb2e78abf6cb243bb5
sahoooii <saho.chu@gmail.com>
2023-10-12 12:01:46 +0900

    Created Models

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,40 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema(
+	{
+		firstname: {
+			type: String,
+			required: true,
+		},
+		lastname: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		picturePath: {
+			type: String,
+			default: '',
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+			default: false,
+		},
+		wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
