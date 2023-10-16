05f8611b4d3fb6d3ad95d0e149724b0708313323
sahoooii <saho.chu@gmail.com>
2023-10-16 15:40:16 +0900

    Separated updateCart function at cartUtils.js

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,12 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart')
 	? JSON.parse(localStorage.getItem('cart'))
 	: { cartItems: [] };
-
-const addDecimals = (num) => {
-	return (Math.round(num * 100) / 100).toFixed(2);
-};
 
 const cartSlice = createSlice({
 	name: 'cart',
@@ -27,27 +24,7 @@ const cartSlice = createSlice({
 			} else {
 				state.cartItems = [...state.cartItems, item];
 			}
-
-			// Calculate items price(total price without any fee)
-			state.itemsPrice = addDecimals(
-				state.cartItems.reduce(
-					(acc, item) => acc + item.price * item.quantity,
-					0
-				)
-			);
-			// Calculate shipping price
-			// If order is over $100 then free, else $10
-			state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-			// Calculate tax price(15% Tax)
-			state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
-			// Calculate total price
-			state.totalPrice = (
-				Number(state.itemsPrice) +
-				Number(state.shippingPrice) +
-				Number(state.taxPrice)
-			).toFixed(2);
-
-			localStorage.setItem('cart', JSON.stringify(state));
+			return updateCart(state);
 		},
 	},
 });
