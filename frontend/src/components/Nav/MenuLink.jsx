30daaf43729a5b6e5a2e14ffa85cf2d9595808bd
sahoooii <saho.chu@gmail.com>
2023-12-13 18:11:09 +0900

    Recreating nav nodal

--- frontend/src/components/Nav/MenuLink.jsx
+++ frontend/src/components/Nav/MenuLink.jsx
@@ -5,12 +5,17 @@ import {
 	Menu,
 	IconButton,
 	Avatar,
+	Box,
+	MenuList,
+	Stack,
+	Typography,
 } from '@mui/material';
 import {
 	PersonOutline,
 	ManageSearchOutlined,
 	Logout,
 	FavoriteBorderOutlined,
+	CloseOutlined,
 } from '@mui/icons-material';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -18,6 +23,8 @@ import { useLogoutMutation } from '../../slices/usersApiSlice';
 import { logout } from '../../slices/authSlice';
 import { resetCart } from '../../slices/cartSlice';
 import { useState } from 'react';
+import styled from '@emotion/styled';
+import { shades } from '../../theme';
 
 const MenuLink = ({ style, width, height }) => {
 	const { userInfo } = useSelector((state) => state.auth);
@@ -26,6 +33,14 @@ const MenuLink = ({ style, width, height }) => {
 
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
+	const [isMenuToggled, setIsMenuToggled] = useState(false);
+
+	const FlexBox = styled(Box)`
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	`;
 
 	const [anchorEl, setAnchorEl] = useState(null);
 	const open = Boolean(anchorEl);
@@ -57,6 +72,111 @@ const MenuLink = ({ style, width, height }) => {
 	return (
 		<>
 			<IconButton
+				onClick={() => setIsMenuToggled(!isMenuToggled)}
+				sx={{ p: 0 }}
+				style={style}
+			>
+				<Avatar
+					src={userInfo.picturePath}
+					alt={fullName}
+					sx={{ width: { width }, height: { height } }}
+				/>
+			</IconButton>
+
+			{/* Overlay */}
+			<Box
+				display={isMenuToggled ? 'block' : 'none'}
+				backgroundColor='rgba(0,0,0,0.4)'
+				position='fixed'
+				zIndex={10}
+				width='100%'
+				height='100%'
+				left='0'
+				top='0'
+				overflow='auto'
+			>
+				{/* Modal */}
+
+				<Box
+					position='fixed'
+					right='0'
+					bottom='0'
+					height='100%'
+					backgroundColor='white'
+					sx={{ width: { xs: 1, sm: 'max(350px, 25%)' } }}
+				>
+					<Box padding='30px' overflow='auto' height='100%'>
+						{/* Header */}
+						<Box display='flex' justifyContent='flex-end' mb='15px' mr='20px'>
+							<IconButton onClick={() => setIsMenuToggled(!isMenuToggled)}>
+								<CloseOutlined />
+							</IconButton>
+						</Box>
+
+						{/* Menu */}
+						<Box>
+							<MenuList>
+								<Stack spacing={2}>
+									<FlexBox>
+										<Link to='/myprofile' style={{ textDecoration: 'none' }}>
+											<MenuItem style={{ alignItems: 'center' }}>
+												<ListItemIcon>
+													<PersonOutline
+														sx={{ marginRight: '6px', fontSize: '20px' }}
+													/>{' '}
+												</ListItemIcon>
+												<Typography
+													variant='h3'
+													sx={{ color: 'rgba(0, 0, 0, 0.54)' }}
+												>
+													{fullName}
+												</Typography>
+											</MenuItem>
+										</Link>
+									</FlexBox>
+									<FlexBox>
+										<Link to='/orderhistory' style={{ textDecoration: 'none' }}>
+											<MenuItem
+												sx={{ justifyContent: 'space-between' }}
+												onClick={handleClose}
+											>
+												<ListItemIcon>
+													<ManageSearchOutlined sx={{ marginRight: '8px' }} />{' '}
+													Order History
+												</ListItemIcon>
+											</MenuItem>
+										</Link>
+									</FlexBox>
+									<FlexBox>
+										<Link to='/wishlist' style={{ textDecoration: 'none' }}>
+											<MenuItem
+												sx={{ justifyContent: 'space-between' }}
+												onClick={handleClose}
+											>
+												<ListItemIcon>
+													<FavoriteBorderOutlined sx={{ marginRight: '8px' }} />{' '}
+													Wish List
+												</ListItemIcon>
+											</MenuItem>
+										</Link>
+									</FlexBox>
+
+									<Divider />
+									<MenuItem
+										sx={{ justifyContent: 'space-between', mt: 0.5 }}
+										onClick={logoutHandler}
+									>
+										<ListItemIcon>
+											<Logout sx={{ marginRight: '8px' }} /> Logout
+										</ListItemIcon>
+									</MenuItem>
+								</Stack>
+							</MenuList>
+						</Box>
+					</Box>
+				</Box>
+			</Box>
+			{/* <IconButton
 				onClick={handleClick}
 				aria-controls={open ? 'menu-appBar' : undefined}
 				aria-haspopup='true'
@@ -69,9 +189,9 @@ const MenuLink = ({ style, width, height }) => {
 					alt={fullName}
 					sx={{ width: { width }, height: { height } }}
 				/>
-			</IconButton>
+			</IconButton> */}
 
-			<Menu
+			{/* <Menu
 				sx={{
 					mt: { xs: -3.5, sm: 1 },
 					ml: { xs: 1 },
@@ -134,7 +254,7 @@ const MenuLink = ({ style, width, height }) => {
 						<Logout sx={{ marginRight: '8px' }} /> Logout
 					</ListItemIcon>
 				</MenuItem>
-			</Menu>
+			</Menu> */}
 		</>
 	);
 };
