0f0e90659c821a99a4302efcab5e7980f666ece6
sahoooii <saho.chu@gmail.com>
2024-01-02 18:38:46 +0900

    Creating orderlist table

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -9,8 +9,12 @@ import {
 	TableRow,
 	Paper,
 	Typography,
+	styled,
+	tableCellClasses,
 } from '@mui/material';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import { useGetOrdersQuery } from '../../slices/ordersApiSlice';
+import { shades } from '../../theme';
 import Loader from '../../components/Utils/Loader';
 import Message from '../../components/Utils/Message';
 
@@ -32,11 +36,31 @@ const OrderListScreen = () => {
 		},
 	];
 
+	const StyledTableCell = styled(TableCell)(({ theme }) => ({
+		[`&.${tableCellClasses.head}`]: {
+			backgroundColor: shades.blue[400],
+			color: 'white',
+		},
+		[`&.${tableCellClasses.body}`]: {
+			fontSize: 14,
+		},
+	}));
+
+	const StyledTableRow = styled(TableRow)(({ theme }) => ({
+		'&:nth-of-type(odd)': {
+			backgroundColor: theme.palette.action.hover,
+		},
+		// hide last border
+		'&:last-child td, &:last-child th': {
+			border: 0,
+		},
+	}));
+
 	return (
 		<Box
 			sx={{
 				m: { md: '30px auto', xs: '10px auto' },
-				width: { xs: '95%', sm: '90%' },
+				width: { xs: '95%', sm: '95%' },
 			}}
 		>
 			<Typography variant='h3' sx={{ mb: '20px' }}>
@@ -53,20 +77,57 @@ const OrderListScreen = () => {
 				<>
 					<Paper sx={{ width: '100%', overflow: 'hidden' }}>
 						<TableContainer sx={{ maxHeight: 440 }}>
-							<Table stickyHeader aria-label='sticky table'>
+							<Table
+								stickyHeader
+								aria-label='sticky table'
+								sx={{ minWidth: 654 }}
+							>
 								<TableHead>
 									<TableRow>
 										{columns.map((column) => (
-											<TableCell
+											<StyledTableCell
 												key={column.id}
 												align={column.align}
 												style={{ minWidth: column.minWidth }}
+												sx={{ fontWeight: 'bold' }}
 											>
 												{column.label}
-											</TableCell>
+											</StyledTableCell>
 										))}
 									</TableRow>
 								</TableHead>
+
+								<TableBody>
+									{orders.map((order) => (
+										<StyledTableRow key={order._id} hover>
+											<StyledTableCell>{order._id}</StyledTableCell>
+											<StyledTableCell>
+												{order.user &&
+													`${order.user.firstName} ${order.user.lastName}`}
+											</StyledTableCell>
+											<StyledTableCell align='right'>
+												{order.createdAt.substring(0, 10)}
+											</StyledTableCell>
+											<StyledTableCell align='right'>
+												$ {order.totalPrice}
+											</StyledTableCell>
+											<StyledTableCell align='right'>
+												{order.isPaid ? (
+													order.paidAt.substring(0, 10)
+												) : (
+													<CloseOutlinedIcon sx={{ color: '#FF0000' }} />
+												)}
+											</StyledTableCell>
+											<StyledTableCell align='right'>
+												{order.isDelivered ? (
+													order.deliveredAt.substring(0, 10)
+												) : (
+													<CloseOutlinedIcon sx={{ color: '#FF0000' }} />
+												)}
+											</StyledTableCell>
+										</StyledTableRow>
+									))}
+								</TableBody>
 							</Table>
 						</TableContainer>
 					</Paper>
