157d1cd9b9acd084fc137825f58a527cfd6c3245
sahoooii <saho.chu@gmail.com>
2025-03-31 16:31:31 +0900

    Setting vercel and render url

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ app.use(
 		origin: [
 			'http://localhost:3000', // Front-end(for local)
 			'http://127.0.0.1:3000', // local other ver.
-			'https://athleisurestyles.onrender.com', // deploy
+			'https://athleisurestyles.onrender.com', // render
+			'https://mern-athleisure-styles.vercel.app', //vercel
 		],
 		methods: ['GET', 'POST', 'PUT', 'DELETE'], // ✅ 許可するメソッドAllow method
 		credentials: true, // Auth（Cookie, JWT など）
